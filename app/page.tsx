8e1c64d3c75c1a5dcdd26f069ab425c91df2b61e
Igor Feoktistov <feoktist92@gmail.com>
2023-10-11 20:51:13 +0800

    styles changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React, { useRef, useState, useEffect } from 'react';
+
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -20,21 +21,9 @@ ChartJS.register(
   Tooltip,
   Legend
 );
-const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Статистика',
-    },
-  },
-};
 
 export default function Home() {
-  const selectRef = useRef<HTMLSelectElement>(null)
+  const selectRef = useRef<HTMLSelectElement>(null);
   const [items, setItems] = useState([
     {finance: {
       periods: [
@@ -44,19 +33,79 @@ export default function Home() {
           month: {}
         }}]
   }}]);
-  const [selectedOption, setSelectedOption] = useState( 'За последний год');
-
+  const [selectedOption, setSelectedOption] = useState('За последний год');
   const [chartData, setChartData] = useState<{
     labels: string[];
     datasets: {
       label: string;
       data: number[];
-      backgroundColor: string;
+      backgroundColor: string,
+      borderRadius: number,
+      barThickness: number
     }[];
   }>({
     labels: [],
     datasets: [],
   });
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: true,
+        intersect: true,
+        position: 'average',
+        backgroundColor: '#65FF8E',
+        callbacks: {
+          title: () => '',
+          label: function (context:any) {
+            return context.parsed.y.toString();
+          }
+        },
+        displayColors: false,
+        bodyFont: {
+          family: 'Manrope',
+          size: 16,
+          lineHeight: 1.5,
+        },
+        bodyColor: '#000',
+        padding: {
+          left: 8,
+          right: 8
+      }
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+        ticks: {
+          color: '#000',
+          font: {
+            size: 20,
+            family: 'Manrope',
+          },
+          maxRotation: 0,
+        },
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+        ticks: {
+          color: '#000',
+          font: {
+            size: 20,
+            family: 'Manrope',
+          },
+        },
+      },
+    },
+  }
 
 
   React.useEffect(()=> {
@@ -77,7 +126,9 @@ export default function Home() {
           {
             label: selectedOption,
             data: Object.values(year).map((value) => Number(value)),
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            backgroundColor: '#000AFF',
+            borderRadius: 4,
+            barThickness: 16,
           },
         ],
       });
@@ -89,23 +140,28 @@ export default function Home() {
           {
             label: selectedOption,
             data: Object.values(halfyear).map((value) => Number(value)),
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            backgroundColor: '#000AFF',
+            borderRadius: 4,
+            barThickness: 16,
           },
         ],
       });
       break;
     case 'За последний месяц':
       setChartData({
-        labels: Object.keys(month),
+        labels:  ['01',' ',' ', ' ', '05', ' ', ' ',  ' ', '', '10', '',' ',' ', ' ', '15', ' ', ' ',  ' ', ' ', '20', '', '', '', ' ', '25', ' ', ' ',  ' ', '', '30', ''],
         datasets: [
           {
             label: selectedOption,
             data: Object.values(month).map((value) => Number(value)),
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            backgroundColor: '#000AFF',
+            borderRadius: 4,
+            barThickness: 16,
           },
         ],
       });
       break;
+     
     default:
       setChartData({
         labels: [],
@@ -118,8 +174,9 @@ export default function Home() {
 
   return (
   <div className='wrapper'>
+    <div className='select-wrapper'>
        <select
-        name="date"
+        name='date'
         ref={selectRef}
         onChange={(e) => setSelectedOption(e.target.value)}
       >
@@ -127,6 +184,10 @@ export default function Home() {
         <option value="За последние полгода">За последние полгода</option>
         <option value="За последний месяц">За последний месяц</option>
       </select>
-      {Object.values(year)?.length > 0 && <Bar options={options} data={chartData} />} 
-  </div>)
+      </div>
+      {Object.values(year)?.length > 0 && <Bar
+       //@ts-ignore options={options} 
+       data={chartData}/>} 
+      </div>
+  )
 }
